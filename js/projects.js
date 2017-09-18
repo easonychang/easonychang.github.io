c9b3a52e45cb889a77cf285bcc9598dc3063a864
easonychang <easonchangar1997@gmail.com>
2017-09-18 10:55:03 -0700

    tip easy project card added, project javascript updated

--- js/projects.js
+++ js/projects.js
@@ -12,4 +12,18 @@ $(document).ready(function(){
         $(".slide-down").show();
         $(".slide-up").hide();
     });
+
+    $(".slide-down-two").click(function(){
+        $(".long-descrip-two").slideDown("slow");
+        $(".short-two").hide();
+        $(".slide-down-two").hide();
+        $(".slide-up-two").show();
+    });
+
+    $(".slide-up-two").click(function(){
+        $(".long-descrip-two").slideUp(1);
+        $(".short-two").show();
+        $(".slide-down-two").show();
+        $(".slide-up-two").hide();
+    });
 });
